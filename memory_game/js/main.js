a690837f6d824ea7c7870d8d897782a38c19cd90
Catherine Guo <catguo88@gmail.com>
2018-07-15 20:22:00 -0400

    memory game version with basic functionality

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,12 +31,16 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+
 	console.log("User flipped " + cards[cardId].rank);
 	console.log("Image: " + cards[cardId].cardImage);
 	console.log("Suit: " + cards[cardId].suit);
 
 	cardsInPlay.push(cards[cardId].rank);
+
+	this.setAttribute('src', cards[cardId].cardImage);
 	
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
@@ -48,5 +52,14 @@ var flipCard = function(cardId) {
 	}
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
